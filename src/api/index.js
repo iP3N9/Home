52b3e5f75bab20d1f273404e4b761a20531f4e78
M3D4 <ehy@qq.com>
2023-08-30 15:17:03 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -61,7 +61,7 @@ export const getAdcode = async (key) => {
 // 获取高德地理天气信息
 export const getWeather = async (key, adcode) => {
   const res = await fetch(
-    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&city=${city}`,
+    `https://restapi.amap.com/v3/weather/weatherInfo?key=${key}&adcode=${city}`,
   );
   return await res.json();
 };
